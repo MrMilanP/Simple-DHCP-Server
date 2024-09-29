8b73f2fcaeeb0385b5a73880cf928b618ed65660
Milan <88509341+MrMilanP@users.noreply.github.com>
2024-09-29 23:40:28 +0200

    Update dhcp.h
    i dalje...

--- Simple DHCP Server/dhcp.h
+++ Simple DHCP Server/dhcp.h
@@ -1,40 +1,40 @@
 #pragma once
 
-#include <winsock.h>
+#include <winsock2.h>  // Promeniti iz <winsock.h> u <winsock2.h>
 #include <cstdint>
 
-#define DHCP_UDP_OVERHEAD	(20 + /* IP header */			8)   /* UDP header */
-#define DHCP_SNAME_LEN		    64
-#define DHCP_FILE_LEN	        128
-#define DHCP_FIXED_NON_UDP	  236
-#define DHCP_FIXED_LEN		  (DHCP_FIXED_NON_UDP + DHCP_UDP_OVERHEAD)
-/* Everything but options. */
-#define BOOTP_MIN_LEN		300
+#define DHCP_UDP_OVERHEAD (20 + 8)   // IP header + UDP header
+#define DHCP_SNAME_LEN    64
+#define DHCP_FILE_LEN     128
+#define DHCP_FIXED_NON_UDP 236
+#define DHCP_FIXED_LEN    (DHCP_FIXED_NON_UDP + DHCP_UDP_OVERHEAD) // Everything but options
+#define BOOTP_MIN_LEN     300
 
-#define DHCP_MTU_MAX		1500
-#define DHCP_MTU_MIN    576
+#define DHCP_MTU_MAX      1500
+#define DHCP_MTU_MIN      576
 
 #define DHCP_MAGIC_COOKIE_LEN 4
 
-#define DHCP_MAX_OPTION_LEN	(DHCP_MTU_MAX - DHCP_FIXED_LEN - DHCP_MAGIC_COOKIE_LEN)
+#define DHCP_MAX_OPTION_LEN (DHCP_MTU_MAX - DHCP_FIXED_LEN - DHCP_MAGIC_COOKIE_LEN)
 #define DHCP_MIN_OPTION_LEN (DHCP_MTU_MIN - DHCP_FIXED_LEN)
 
-#define MAGIC_COOKIE 0x63825363;
+#define MAGIC_COOKIE 0x63825363  // Uklonjena tačka i zapeta
 
-#define BUFLEN 1500	//Max length of buffer
+#define BUFLEN 1500  // Max length of buffer
 #define SERVER_PORT 67	
 #define CLIENT_PORT 68
 
+// DHCP message types
+#define DHCPDISCOVER       0x01
+#define DHCPOFFER          0x02
+#define DHCPREQUEST        0x03
+#define DHCPDECLINE        0x04
+#define DHCPACK            0x05
+#define DHCPNAK            0x06
+#define DHCPRELEASE        0x07
+#define DHCPINFORM         0x08
 
-#define DHCPDISCOVER          0x01
-#define DHCPOFFER             0x02
-#define DHCPREQUEST           0x03
-#define DHCPDECLINE           0x04
-#define DHCPACK               0x05
-#define DHCPNAK               0x06
-#define DHCPRELEASE           0x07
-#define DHCPINFORM            0x08
-
+// DHCP options
 #define DHCP_PADDING          0x00
 #define DHCP_SUBNET           0x01
 #define DHCP_TIME_OFFSET      0x02 /* (localtime - UTC_time) in seconds. signed */
@@ -42,7 +42,7 @@
 #define DHCP_TIME_SERVER      0x04 /* RFC 868 time server (32-bit, 0 = 1.1.1900) */
 #define DHCP_NAME_SERVER      0x05 /* IEN 116 _really_ ancient kind of NS */
 #define DHCP_DNS_SERVER       0x06
-#define DHCP_LOG_SERVER       0x07 /* port 704 UDP log (not syslog)
+#define DHCP_LOG_SERVER       0x07  /* port 704 UDP log (not syslog) */
 #define DHCP_COOKIE_SERVER    0x08 /* "quote of the day" server */
 #define DHCP_LPR_SERVER       0x09
 #define DHCP_HOST_NAME        0x0c /* either client informs server or server gives name to client */
@@ -81,50 +81,42 @@
 #define DHCP_WPAD             0xfc /* MSIE's Web Proxy Autodiscovery Protocol */
 #define DHCP_END              0xff
 
-
 class Dhcp
 {
-	private:
-		int x;
+private:
+    int x;
 
-		SOCKET s;
-		struct sockaddr_in server, si_other;
-		int slen, recv_len;
-		byte buf[BUFLEN];
-		WSADATA wsa;
+    SOCKET s;
+    struct sockaddr_in server, si_other;
+    int slen, recv_len;
+    uint8_t buf[BUFLEN]; // Zamenjen byte sa uint8_t
+    WSADATA wsa;
 
-		struct  Dhcp_packet {
-			u_char  op;	//0	
-			u_char  htype;	//1 
-			u_char  hlen;		//2 
-			u_char  hops;	//3	
-			uint32_t xid;	//4	
-			uint16_t secs;	//8	
-			uint16_t flags;	//10
-			struct in_addr ciaddr; //12
-			struct in_addr yiaddr;	//16
-			struct in_addr siaddr;	//20
-			struct in_addr giaddr;	//24
-			u_char chaddr[16];	//28
-			u_char sname[64];	//44
-			u_char file[128];	//108
-			u_char options[DHCP_MIN_OPTION_LEN];//236			
-		} ;
+    struct Dhcp_packet {
+        u_char  op;	//0	
+        u_char  htype;	//1 
+        u_char  hlen;	//2 
+        u_char  hops;	//3	
+        uint32_t xid;	//4	
+        uint16_t secs;	//8	
+        uint16_t flags;	//10
+        struct in_addr ciaddr; //12
+        struct in_addr yiaddr;	//16
+        struct in_addr siaddr;	//20
+        struct in_addr giaddr;	//24
+        u_char chaddr[16];	//28
+        u_char sname[64];	//44
+        u_char file[128];	//108
+        u_char options[DHCP_MIN_OPTION_LEN]; //236			
+    };
 
-		void parseOptions(u_char options[]);
+    void parseOptions(u_char options[]);
 
+public:
+    Dhcp_packet dhcp_packet;
 
+    Dhcp();
 
-	public:
-
-		Dhcp_packet dhcp_packet;
-
-		Dhcp();
-
-		void setX(int i);
-
-		int getX();
-
-
+    void setX(int i);
+    int getX();
 };
-
